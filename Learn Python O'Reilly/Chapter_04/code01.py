98bbcb5c7fa2c8dc64e3335229564d427cfe1e9a
daniel.savescu <savescu.daniel.cristian@gmail.com>
2024-06-11 15:41:29 +0300

    done with numbers introduction

--- Learn Python O'Reilly/Chapter_04/code01.py
+++ Learn Python O'Reilly/Chapter_04/code01.py
@@ -1,4 +1,7 @@
 import sys
+import math
+import random
+
 
 sys.set_int_max_str_digits(0)
 
@@ -9,3 +12,13 @@ print(1.5 * 4) #floating-point multiplication
 print(2 ** 100) #2 to the power of 100, again
 
 print(len(str(2 ** 1000000)))
+
+print(3.1415 * 2)
+
+print(math.pi)
+
+print(math.sqrt(85))
+
+print(random.random())
+
+print(random.choice([1,2,3,4]))
