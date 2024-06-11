50fafcfd6998b68a1bcb1c5a49f4ffa909e9f657
daniel.savescu <savescu.daniel.cristian@gmail.com>
2024-06-11 15:34:19 +0300

    set int max str digits

--- Learn Python O'Reilly/Chapter_04/code01.py
+++ Learn Python O'Reilly/Chapter_04/code01.py
@@ -1,5 +1,11 @@
+import sys
+
+sys.set_int_max_str_digits(0)
+
 print(123 + 222) #integer addition
 
 print(1.5 * 4) #floating-point multiplication
 
 print(2 ** 100) #2 to the power of 100, again
+
+print(len(str(2 ** 1000000)))
