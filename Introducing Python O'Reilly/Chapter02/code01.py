44abb1def9e2723d5bca1ba83885d7e5accc6c72
daniel.savescu <savescu.daniel.cristian@gmail.com>
2024-06-17 13:38:51 +0300

    chapter02 introducing python

---
+++ Introducing Python O'Reilly/Chapter02/code01.py
@@ -0,0 +1,16 @@
+a = 7
+
+print(a)
+
+b = a
+
+print(b)
+
+print(type(a))
+
+print(type(b))
+
+print(type(90.4))
+
+print(type('abc'))
+
---
+++ Introducing Python O'Reilly/Chapter02/code02.py
@@ -0,0 +1,55 @@
+print(123)
+
+print(+123)
+
+print(-123)
+
+print(5+9)
+
+print(10-7)
+
+print(4-10)
+
+print(5+6+5+11+4)
+
+print(6 * 5 * 4 * 2 * 9)
+
+print(9/5) #when dividing integers it results in floating point number
+
+print(9//5) #truncating the result
+
+a = 19
+
+print(a - 5)
+
+print(a)
+
+a = a - 5 #a value will change
+
+print(a)
+
+b = 15
+
+temp = b - 5  
+
+b = temp
+
+print(b)
+
+c = 15
+
+c += 5
+
+print(c)
+
+d = 20
+
+d /= 5
+
+print(d)
+
+print(9%4)
+
+#precedence
+
+print(3 * 4 + 2)
