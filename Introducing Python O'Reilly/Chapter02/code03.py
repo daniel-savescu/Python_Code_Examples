76e1f08cfc68fe0906aae83d351cfaa3a32d2ca3
daniel.savescu <savescu.daniel.cristian@gmail.com>
2024-06-19 12:24:20 +0300

    typecasting and strings introduction

---
+++ Introducing Python O'Reilly/Chapter02/code03.py
@@ -0,0 +1,26 @@
+print(int(True))
+
+print(int(False))
+
+print(int(98.6))
+
+print(int(1.0e4))
+
+print(int('89'))
+
+print(int(-24))
+
+print(int(+12))
+
+print(True + 2)
+
+print(5.0 - False)
+
+print(float(True))
+
+print(float(False))
+
+print(float(98))
+
+print(float('99'))
+
---
+++ Introducing Python O'Reilly/Chapter02/code04.py
@@ -0,0 +1,103 @@
+s1 = "String"
+
+s2 = 'String2'
+
+print(s1)
+
+print(s2)
+
+s3 = 'He sayig "Hello"'
+
+s4 = "Let's go!"
+
+print(s3)
+
+print(s4)
+
+bottles = 99
+
+base = ''
+
+base += 'There are currently : '
+
+base += str(bottles)
+
+base += ' bottles'
+
+print(base)
+
+print(str(True))
+
+print(str(10.4))
+
+print(str(1.0e4))
+
+print("This is\nA new\nLine")
+
+print("a\tb")
+
+print("ab\tc")
+
+print("Please" + " wait")
+
+print("Hey" " stop")
+
+print("olla " * 3)
+
+letters = 'abcdefgh'
+
+print(letters[0])
+
+print(letters[1])
+
+print(letters[-1])
+
+name = "Henny"
+
+print(name.replace('H', 'P'))
+
+print('P' + name[1:])
+
+print(letters[1:])
+
+print(letters[:])
+
+print(letters[-3:])
+
+print(letters[2:-4])
+
+print(letters[::-1])
+
+print(len(letters))
+
+empty = ""
+
+print(len(empty))
+
+todoe = 'get milk, get pasta, get patatoes'
+
+print(todoe.split(','))
+
+print(todoe.split())
+
+names = ['john', 'bill', 'emily']
+
+print(', '.join(names))
+
+s5 = 'test'
+
+print(letters.find(s5))
+
+s6 = 'testing...'
+
+print(s6.strip('.'))
+
+print(s6.capitalize())
+
+print(s6.upper())
+
+print(s6.lower())
+
+print(s6.center(30))
+
+print(s6.ljust(30))
