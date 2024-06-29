2ee7fcc526842cd7e169cab607f659d0e4dce2b6
Daniel Savescu <danielsavescu@Daniels-iMac.local>
2024-06-29 12:04:51 +0300

    whiles, and rage

---
+++ Introducing Python O'Reilly/Chapter03/code04.py
@@ -0,0 +1,17 @@
+for x in range(0,3):
+    print(x)
+
+l = list(range(0,3))
+print(l)
+
+
+for x in range(2,-1,-1):
+    print(x)
+
+l2 = list(range(2,-1,-1))
+
+print(l2)
+
+even = list(range(0, 11, 2))
+
+print(even)
